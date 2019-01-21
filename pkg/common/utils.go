1d1e21a4fd9c43a396832c9a42303478db7d87fa
Jack <jjricci555@gmail.com>
2019-01-21 15:13:15 -0500

    [CONFIG] Refactored LoadConfig() to have generic method in common

--- pkg/common/utils.go
+++ pkg/common/utils.go
@@ -1,8 +1,13 @@
 package common
 
 import (
+	"bufio"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io"
 	"log"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 )
@@ -32,4 +37,49 @@ func ChoosePort(ports []string) string {
 	rand.Seed(time.Now().UnixNano())
 	port := ports[rand.Intn(numPorts)]
 	return ConstructPort(port)
+}
+
+/**
+	Read the server configuration for the
+	master from config file
+ */
+func LoadConfig(path string, c interface{}) {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal("ERR: Unable to read config file. Terminating...")
+	}
+	defer f.Close()
+
+	fileinfo, err := f.Stat()
+	if err != nil {
+		log.Fatalln("ERR: Unable to stat config file info: ", err)
+	}
+
+	filesize := fileinfo.Size()
+	b := make([]byte, filesize)
+
+	br := bufio.NewReader(f)
+	log.Println("LOAD_CONFIG(): Started new buffered reader...")
+	for {
+		n, err := br.Read(b)
+		if err != nil {
+			if err != io.EOF {
+				log.Fatalln("ERR: While reading from file: ", err)
+			}
+			log.Println("Reached EOF. Breaking...")
+			break;
+		}
+
+		log.Println("LOAD_CONFIG(): Number of bytes read: ", n)
+		log.Println("LOAD_CONFIG(): Byte slice read from file is: ", string(b[:n]))
+	}
+
+	log.Println("LOAD_CONFIG(): Byte slice read from file is: ", string(b))
+
+	err = yaml.Unmarshal(b, c)
+	if err != nil {
+		log.Fatal("ERR: ", err)
+	}
+
+	fmt.Println("LOAD_CONFIG(): MasterConfig is: ", c)
 }
--- pkg/master/masterUtils.go
+++ pkg/master/masterUtils.go
@@ -1,12 +1,8 @@
 package master
 
 import (
-	"bufio"
 	"fmt"
-	"gopkg.in/yaml.v2"
-	"io"
-	"log"
-	"os"
+	"github.com/justjack555/elevator/pkg/common"
 )
 
 type SelectionConfig struct {
@@ -20,51 +16,14 @@ type MasterConfig struct {
 	Selection_config SelectionConfig
 }
 
-const MaxBYTES int = 4096
+const configPATH string = "cmd/master/master.yaml"
 /**
 	Read the server configuration for the
 	master from config file
  */
 func LoadConfig() *MasterConfig {
 	mc := new(MasterConfig)
-
-
-	f, err := os.Open("cmd/master/master.yaml")
-	if err != nil {
-		log.Fatal("ERR: Unable to read config file. Terminating...")
-	}
-	defer f.Close()
-
-	fileinfo, err := f.Stat()
-	if err != nil {
-		log.Fatalln("ERR: Unable to stat config file info: ", err)
-	}
-
-	filesize := fileinfo.Size()
-	b := make([]byte, filesize)
-
-	br := bufio.NewReader(f)
-	log.Println("LOAD_CONFIG(): Started new buffered reader...")
-	for {
-		n, err := br.Read(b)
-		if err != nil {
-			if err != io.EOF {
-				log.Fatalln("ERR: While reading from file: ", err)
-			}
-			log.Println("Reached EOF. Breaking...")
-			break;
-		}
-
-		log.Println("LOAD_CONFIG(): Number of bytes read: ", n)
-		log.Println("LOAD_CONFIG(): Byte slice read from file is: ", string(b[:n]))
-	}
-
-	log.Println("LOAD_CONFIG(): Byte slice read from file is: ", string(b))
-
-	err = yaml.Unmarshal(b, mc)
-	if err != nil {
-		log.Fatal("ERR: ", err)
-	}
+	common.LoadConfig(configPATH, mc)
 
 	fmt.Println("LOAD_CONFIG(): MasterConfig is: ", *mc)
 	return mc
